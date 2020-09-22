d2d026df42faf290142cc9315545c5c1adb6af5c
hicinformatic <hicinformatic@gmail.com>
2020-09-22 05:30:02 +0200

    better manager

--- managers.py
+++ managers.py
@@ -2,7 +2,6 @@ from django.db import models
 from company.apps import CompanyConfig
 from company import queries
 
-
 Prefetch_related = ('company_%s' % country.lower() for country,table in CompanyConfig.countries.items())
 class CompanyManager(models.Manager.from_queryset(models.QuerySet)):
     def get_queryset(self):
